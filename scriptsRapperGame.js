1ae8c61b6b97f630367b1311e9df15334364b2ec
Your Name <controlsltrapidfire@gmail.comm>
2017-12-29 00:51:47 -0600

    Now how in the hell did I forget to put the god Nas in there

--- scriptsRapperGame.js
+++ scriptsRapperGame.js
@@ -9,70 +9,70 @@ function Rapper(name, birthName, dateOfBirth, song, lyric, picture){
   this.song        = song;
   this.lyric       = lyric;
   this.picture     = picture;
-};
-
-var rapperList = [
-
-var	rapper1	 =
+}
+let	rapper1	 = new Rapper
   ("Pimp C","Chad Butler","December 29, 1973","Port Arthur Texas","some song","some lyric","some picture");
-var	rapper2	 =
+let	rapper2	 = new Rapper
   ("Gucci Mane","Radric Davis","February 12, 1980","Atlanta, Georgia","some song","some lyric","some picture");
-var	rapper3	 =
+let	rapper3	 = new Rapper
   ("Rakim","William Griffin, Jr.","January 28, 1968","New York, New York","some song","some lyric","some picture");
-var	rapper4	 =
+let	rapper4	 = new Rapper
   ("Big Daddy Kane","Antonio Hardy","September 10, 1968","New York, New York","some song","some lyric","some picture");
-var	rapper5	 =
+let	rapper5	 = new Rapper
   ("Future","Nayvadius Wilburn","November 20, 1983","Atlanta, Georgia","some song","some lyric","some picture");
-var	rapper6	 =
+let	rapper6	 = new Rapper
   ("Drake","Aubrey Graham","October 24,1986","Toronto, Canada","some song","some lyric",	"some picture");
-var	rapper7	 =
+let	rapper7	 = new Rapper
   ("21 Savage","Shayaa Bin Abraham-Joseph","October 22, 1992","Atlanta, Georgia","some song","some lyric","some picture");
-var	rapper8	 =
+let	rapper8	 = new Rapper
   ("Lil Kim","Kimbery Jones","July 11, 1975","New York, New York","some song","some lyric",	"some picture");
-var	rapper9	 =
+let	rapper9	 = new Rapper
   ("Nicki Manaj","Onika Maraj",	"December 8, 1982","New York, New York","some   song",	"some lyric","some picture");
-var	rapper10	=
+let	rapper10	= new Rapper
   ("MC Lyte","Lana Moorer","October 11, 1970","New York, New York","some song","some lyric",	"some picture");
-var	rapper11	=
+let	rapper11	= new Rapper
   ("Mia X","Mia Young","January 9, 1970","New Orleans, Louisiana","some song","some lyric","some picture");
-var	rapper12	=
+let	rapper12	= new Rapper
   ("Cardi B","Belcalis Almanzar","October 11, 1992","New York, New York","some song","some lyric","some picture");
-var	rapper13	=
+let	rapper13	= new Rapper
   ("Kanye West","Kanye West","June 8, 1977","Chicago, Illinois","some song",	"some lyric","some picture");
-var	rapper14	=
+let	rapper14	= new Rapper
   ("Jay-Z","Shawn Carter","December 4, 1969","New York, New York","some song",	"some lyric","some picture")	;
-var	rapper15	=
+let	rapper15	= new Rapper
   ("RA the Rugged Man","R.A. Thorburn","January 10, 1974","New York, New York",	"some song","some lyric","some picture")	;
-var	rapper16	=
+let	rapper16	= new Rapper
   ("Wise Intelligent","Timothy Grimes","unlisted","New Brunswick, New Jersey",	"some song","some lyric","some picture");
-var	rapper17	=
+let	rapper17	= new Rapper
   ("MC Ren","Lorenzo Matterson","June 16, 1969","Compton, California","some song","some lyric","some picture")	;
-var	rapper18	=
+let	rapper18	= new Rapper
   ("The DOC","Tracy Curry","June 10, 1968","Dallas, Texas","some song","some lyric","some picture");
-var	rapper19	=
+let	rapper19	= new Rapper
   ("Chance the Rapper","Chancelor Bennent","April 16, 1993","Chicago, Illinois","some song","some lyric","some picture");
-var	rapper20	=
+let	rapper20	= new Rapper
   ("Big Boi","Antwan Patton","February 1, 1975","Atlanta, Georgia","some song",	"some lyric","some picture");
-var	rapper21	=
+let	rapper21	= new Rapper
   ("Ghostface Killah","Dennis Coles ","May 6, 1970","New York, New York","some song","some lyric","some picture");
-var	rapper22	=
+let	rapper22	= new Rapper
   ("Prodigy","Albert Johnson","November 2, 1974","Hempstead, New York","some song","some lyric","some picture");
-var	rapper23	=
+let	rapper23	= new Rapper
   ("MF Doom","Daniel Dumile","January 9, 1971","New York, New York","some song","some lyric","some picture");
-var	rapper24	=
+let	rapper24	= new Rapper
   ("Master P","Percy Miller","April 29, 1970","New Orleans, Louisiana","some song","some lyric","some picture");
-var	rapper25	=
+let	rapper25	= new Rapper
   ("Ice Cube","O'Shea Jackson, Sr.","June 15, 1969","Los Angeles, California",	"some song","some lyric","some picture");
-var	rapper26	=
+let	rapper26	= new Rapper
   ("LL Cool J","James Todd Smith","January 14, 1968","New York, New York","some song","some lyric","some picture");
-var	rapper27	=
+let	rapper27	= new Rapper
   ("Jaden Smith","Jaden Smith","July 8, 1998","Malibu, California","some song",	"some lyric","some picture")	;
-var	rapper28	=
+let	rapper28	= new Rapper
   ("Bushwick Bill","Richard Shaw","December 8, 1968","Jamaica","some song","some lyric","some picture");
-var	rapper29	=
+let	rapper29	= new Rapper
   ("2Pac","Lesane Parish Crooks","June 16, 1971","New York, New York","some song","some lyric","some picture");
-var	rapper30	=
+let	rapper30	= new Rapper
   ("Nick Grant","Nick Grant","July 27, 1998","Walterboro, South Carolina","some song","some lyric","some picture");
-]
+Let rapper 31 = new Rapper
+  ("Nas","Nasir Jones","July 27, 1998","New York, New York","some song","some lyric","some picture");
+let rapper 32 = new rapper
+  ("Nas","Nasir Jones","July 27, 1998","New York, New York","some song","some lyric","some picture");
 
-console.log(this.rapper28.name);
+console.log(rapper16);
